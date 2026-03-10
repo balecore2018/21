cdc83683df7cb2edd9c8e38a15c7bcd36ad7ae8e
dima <balecored@gmail.com>
2026-03-10 16:12:41 +0500

    Реализованная страница Main. Shapovalov

---
+++ 21/Pages/Main.xaml.cs
@@ -0,0 +1,49 @@
+﻿using _21.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _21.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CreatedUI();
+        }
+
+        public void CreatedUI()
+        {
+            Parent.Children.Clear();
+            foreach (DocumentContext document in MainWindow.init.AllDocuments)
+                Parent.Children.Add(new Elements.Item(document));
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.add);
+        }
+
+
+    }
+}

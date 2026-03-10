d4d878b6bd6458a40779947cc027ee89a45bdcee
dima <balecored@gmail.com>
2026-03-10 16:01:01 +0500

    Реализованные пользовательские элементы управления. Шаповалов.

---
+++ 21/Elements/Item.xaml.cs
@@ -0,0 +1,51 @@
+﻿using _21.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _21.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        DocumentContext Document;
+        public Item(DocumentContext Document)
+        {
+            InitializeComponent();
+
+            img.Source = new BitmapImage(new Uri(Document.src));
+            lName.Content = Document.name;
+            lUser.Content = $"Ответственный: {Document.user}";
+            lCode.Content = $"Код документа: {Document.id_document}";
+            lUser.Content = $"Дата поступления: {Document.date.ToString("dd.MM.yyyy")}";
+            lStatus.Content = Document.status == 0 ? $"Статус: входящий" : $"Статус: исходящий";
+            lDirect.Content = "Направление: " + Document.vector;
+            this.Document = Document;
+        }
+
+        private void EditDocument(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.frame.Navigate(new Pages.Add(Document));
+        }
+
+        private void DeleteDocument(object sender, RoutedEventArgs e)
+        {
+            Document.Delete();
+            MainWindow.init.AllDocuments = new DocumentContext().AllDocuments();
+            MainWindow.init.OpenPages(MainWindow.pages.main);
+        }
+    }
+}
